e327871e7b001dc0f1565e3735a30fdd2a1b8546
SLGerrick <slgerrick@gmail.com>
2011-11-16 18:49:09 -0800

    Update src/edu/ucla/cens/budburstmobile/floracaching/FloraCacheEasyLevel.java

--- src/edu/ucla/cens/budburstmobile/floracaching/FloraCacheEasyLevel.java
+++ src/edu/ucla/cens/budburstmobile/floracaching/FloraCacheEasyLevel.java
@@ -1,363 +1,200 @@
-package edu.ucla.cens.budburstmobile.floracaching;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
-import com.google.android.maps.MapView;
-import com.google.android.maps.MyLocationOverlay;
-
-import edu.ucla.cens.budburstmobile.R;
-import edu.ucla.cens.budburstmobile.adapter.MyListAdapterMainPage;
-import edu.ucla.cens.budburstmobile.database.OneTimeDBHelper;
-import edu.ucla.cens.budburstmobile.helper.HelperGpsHandler;
-import edu.ucla.cens.budburstmobile.helper.HelperListItem;
-import edu.ucla.cens.budburstmobile.helper.HelperPlantItem;
-import edu.ucla.cens.budburstmobile.helper.HelperSharedPreference;
-import edu.ucla.cens.budburstmobile.helper.HelperValues;
-import edu.ucla.cens.budburstmobile.lists.ListMain;
-import edu.ucla.cens.budburstmobile.mapview.MapViewMain;
-import edu.ucla.cens.budburstmobile.mapview.MyLocOverlay;
-import edu.ucla.cens.budburstmobile.mapview.SpeciesMapOverlay;
-import edu.ucla.cens.budburstmobile.myplants.PBBPlantList;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class FloraCacheEasyLevel extends MapActivity {
-
-	private boolean mIsBound;
-	
-	private HelperGpsHandler gpsHandler;
-	private boolean mFirstGps;	
-	
-	// Map related variables
-	private LocationManager mLocManager = null;
-	private MapView mMapView = null;
-	private MyLocOverlay mMyOverLay = null;
-	private MapController mMapController = null;
-	
-	private double mLatitude 		= 0.0;
-	private double mLongitude 		= 0.0;
-	
-	private ArrayList<FloracacheItem> mPlantList;
-	private HelperPlantItem pItem;
-	private Drawable mMarker;
-	private int mGroupID;
-	
-	private ServiceConnection mConnection = new ServiceConnection() {
-
-		public void onServiceConnected(ComponentName className, IBinder binder) {
-			gpsHandler = ((HelperGpsHandler.GpsBinder) binder).getService();
-			//Toast.makeText(PBBMapMain.this, "Connected", Toast.LENGTH_SHORT).show();
-		}
-
-		public void onServiceDisconnected(ComponentName className) {
-			gpsHandler = null;
-		}
-	};
-	
-	private void doBindService() {
-		
-		Log.i("K", "BindService");
-		
-		bindService(new Intent(FloraCacheEasyLevel.this, HelperGpsHandler.class), mConnection,
-				Context.BIND_AUTO_CREATE);
-		mIsBound = true;
-		//Toast.makeText(PBBMapMain.this, "bindService", Toast.LENGTH_SHORT).show();
-	
-	}
-	
-	private void doUnbindService() {
-		
-		Log.i("K", "UnBindService");
-		
-		if(mIsBound) {
-			if(mConnection != null) {
-				
-			}
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout
+  xmlns:android="http://schemas.android.com/apk/res/android"
+  android:layout_width="fill_parent"
+  android:layout_height="fill_parent">
+  <LinearLayout
+  	android:layout_width="fill_parent"
+  	android:layout_height="fill_parent"
+  	android:orientation="vertical"
+  	android:weightSum="1">
+  <ScrollView
+  	android:layout_width="fill_parent"
+  	android:layout_height="fill_parent"
+  	android:id="@+id/upper"
+  	android:layout_weight="0.9"
+  >
+  	<LinearLayout
+  		android:layout_width="fill_parent"
+	  	android:layout_height="fill_parent"
+	  	android:orientation="vertical"
+  	>
+  	
+  	<!-- This layout is only for local budburst plant -->
+  	
+  	  <LinearLayout
+  	  	android:layout_width="fill_parent"
+  	  	android:layout_height="wrap_content"
+  	  	android:id="@+id/upper_invisible"
+  	  	android:visibility="gone"
+  	  	android:orientation="vertical"
+  	  	android:layout_margin="5dp"
+  	  >
+  	  	<LinearLayout
+			android:layout_width="fill_parent"
+			android:layout_height="wrap_content"
+			android:layout_marginTop="15dp"
+			android:orientation="horizontal"
+			>
+			<ImageView 
+				android:scaleType="fitCenter"
+				android:paddingLeft="5dp"
+				android:paddingRight="5dp"
+				android:layout_width="90dp" 
+				android:layout_height="90dp" 
+				android:id="@+id/species_image" 
+				android:adjustViewBounds="true"
+				/>		
+			<LinearLayout
+				android:layout_width="wrap_content"
+				android:layout_height="wrap_content"
+				android:layout_marginTop="10dp"
+				android:orientation="vertical"
+			>
+				<TextView
+					android:id="@+id/science_name2"
+					android:layout_width="wrap_content" 
+					android:layout_height="wrap_content"
+					android:textSize="19sp" 
+					android:textColor="@drawable/black" 
+					android:layout_gravity="center"
+					android:textStyle="italic|bold" 
+					android:layout_marginTop="10dp"
+					/>			
 			
-			//Toast.makeText(PBBMapMain.this, "UnbindService", Toast.LENGTH_SHORT).show();
-			unbindService(mConnection);
-			mIsBound = false;
-		}
-	}
-	
-	@Override
-	public void onDestroy() {
-		// when user finish this activity, turn off the gps
-		// if there's a overlay, should call disableCompass() explicitly
-		doUnbindService();
-		if(gpsReceiver != null) {
-			unregisterReceiver(gpsReceiver);
-		}
+				<TextView
+					android:id="@+id/common_name2"
+					android:layout_width="wrap_content" 
+					android:layout_height="wrap_content"
+					android:textSize="17sp" 
+					android:textColor="@drawable/black" 
+					android:layout_gravity="center"
+					android:textStyle="italic|bold" 
+					/>		
+			</LinearLayout>
+		</LinearLayout>
+		<TextView
+			android:layout_marginLeft="5dp"
+			android:layout_marginRight="5dp"
+			android:layout_marginBottom="20dp" 
+			android:layout_marginTop="20dp"
+			android:layout_width="wrap_content" 
+			android:layout_height="wrap_content"
+			android:textColor="@drawable/black" 
+			android:layout_gravity="left"
+			android:text="Notes"
+			android:textSize="17sp"
+			android:id="@+id/text"
+		/>
 		
-		mMyOverLay.disableCompass();
-		mMyOverLay.disableMyLocation();
-	
-		super.onDestroy();
-	}
-	
-	private BroadcastReceiver gpsReceiver = new BroadcastReceiver() {
-
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			// TODO Auto-generated method stub
-			Bundle extras = intent.getExtras();
-			
-			if(extras.getBoolean("signal")) {
-				mLatitude = extras.getDouble("latitude");
-				mLongitude = extras.getDouble("longitude");
-				
-				// convert points into GeoPoint
-			    GeoPoint gPoint = getPoint(mLatitude, mLongitude);
-			    
-			    // center the map
-			    if(mFirstGps) {
-			    	mMapController.setCenter(gPoint);
-			    	mFirstGps = false;
-			    }
-			}
-			// if Gps signal is bad
-			else {
-				//Toast.makeText(FloraCacheEasyLevel.this, getString(R.string.Low_GPS_Signal), Toast.LENGTH_SHORT).show();
-			}
-		}	
-	};
-	
-
-	private GeoPoint getPoint(double lat, double lon) {
-		return(new GeoPoint((int)(lat*1000000.0), (int)(lon*1000000.0)));
-	}
-
-	
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	
-	    setContentView(R.layout.floracache_map);
-	    
-	    Intent gIntent = getIntent();
-	    mGroupID = gIntent.getExtras().getInt("group_id");
-		
-		mLocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		// Set MapView
-		mMapView = (MapView)findViewById(R.id.map);
-		mMapView.setBuiltInZoomControls(true);
-		mMapView.setSatellite(false);
-		// Set mapController
-		mMapController = mMapView.getController();
-		mMapController.setZoom(12);
-		
-		// Add mylocation overlay
-		mMyOverLay = new MyLocOverlay(FloraCacheEasyLevel.this, mMapView);
-		mMyOverLay.enableMyLocation();
-//		mMyOverLay.
-		mMyOverLay.enableCompass();
-	    
-		mFirstGps = true;
-		
-		// remove view of accuracy bar
-		TextView titleBar = (TextView)findViewById(R.id.myloc_accuracy);
-		titleBar.setVisibility(View.GONE);
-
-		// initialize plantList
-		mPlantList = new ArrayList<FloracacheItem>();
-		
-		// initialize marker
-		mMarker = getResources().getDrawable(R.drawable.marker);
-		mMarker.setBounds(0, 0, mMarker.getIntrinsicWidth(), mMarker.getIntrinsicHeight());
-		
-		IntentFilter inFilter = new IntentFilter(HelperGpsHandler.GPSHANDLERFILTER);
-		registerReceiver(gpsReceiver, inFilter);
-		Log.i("K", "Receiver Register");
-		
-		checkGpsIsOn();
-		
-		
-	    // TODO Auto-generated method stub
-	}
-	
-	public void checkGpsIsOn() {
-		// check if GPS is turned on...
-		if (mLocManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)) {
-			
-			Location lastLoc = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-			if(lastLoc != null) {
-				mLatitude = lastLoc.getLatitude();
-				mLongitude = lastLoc.getLongitude();
-				mMapController.setCenter(getPoint(mLatitude, mLongitude));
-			}
-			
-			// bind GPS service to current activity
-			doBindService();
-			// call plantlists from the database
-			showSpeciesOnMap(false);
-			
-		}
-		else {
-		   	
-		 new AlertDialog.Builder(FloraCacheEasyLevel.this)
-		   		.setTitle(getString(R.string.Turn_on_GPS))
-		   		.setMessage(getString(R.string.Message_locationDisabledTurnOn))
-		   		.setPositiveButton(getString(R.string.Button_yes), new DialogInterface.OnClickListener() {
-		   			public void onClick(DialogInterface dialog, int whichButton) {
-		   				Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
-		   				startActivityForResult(intent, 1);
-		   			}
-		   		})
-		   		.setNegativeButton(getString(R.string.Button_no), new DialogInterface.OnClickListener() {
-		   			public void onClick(DialogInterface dialog, int whichButton) {
-		   				finish();
-		   			}
-		   		})
-		   		.show();
-		}	
-	}
-	
-	@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1) {
-        	if(resultCode == RESULT_OK) {
-        		Log.i("K", "onActivityResult");
-            	doBindService();
-            	showSpeciesOnMap(false);
-        	}
-        }
-    }
-
-	public void showSpeciesOnMap(boolean hasHandler) {
-		
-		// TODO Auto-generated method stub
-		//otDBH = new OneTimeDBHelper(PBBMapMain.this);
-		
-		GeoPoint gPoint = new GeoPoint((int)(mLatitude * 1000000), (int)(mLongitude * 1000000));
-		
-		mMapView.setBuiltInZoomControls(true);
-		mMapView.invalidate();
-		
-		getMyListFromServer();
-
-	}
-	
-	private void getMyListFromServer() {
-		HelperSharedPreference hPref = new HelperSharedPreference(this);
-		if(!hPref.getPreferenceBoolean("floracache")) {
-			Toast.makeText(FloraCacheEasyLevel.this, "To download the list, Go 'Settings' page", Toast.LENGTH_SHORT).show();
-		}
-		else {
-			OneTimeDBHelper oDBH = new OneTimeDBHelper(this);
-			mPlantList = oDBH.getFloracacheLists(FloraCacheEasyLevel.this, HelperValues.FLORACACHE_EASY, mGroupID, mLatitude, mLongitude);
-		
-			GeoPoint gPoint = new GeoPoint((int)(mLatitude * 1000000), (int)(mLongitude * 1000000));
-			
-			mMapView.getOverlays().add(mMyOverLay);
-			mMapView.getOverlays().add(new FloraCacheOverlay(mMapView, mMarker, mPlantList));
-			
-						
-			
-			mMapController.setCenter(gPoint);
-			mMapController.setZoom(18);
-		}
-	}
-	
-	private void refreshList() {
-		OneTimeDBHelper oDBH = new OneTimeDBHelper(this);
-		mPlantList = oDBH.getFloracacheLists(FloraCacheEasyLevel.this, HelperValues.FLORACACHE_EASY, mGroupID, mLatitude, mLongitude);
-	
-		mMapView.getOverlays().clear();
-		mMapView.invalidate();
-		
-		GeoPoint gPoint = new GeoPoint((int)(mLatitude * 1000000), (int)(mLongitude * 1000000));
-		
-		mMapView.getOverlays().add(new FloraCacheOverlay(mMapView, mMarker, mPlantList));
-		mMapView.getOverlays().add(mMyOverLay);
-				
-		
-		mMapController.setCenter(gPoint);
-	}
-	
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	public boolean onCreateOptionsMenu(Menu menu){
-		super.onCreateOptionsMenu(menu);
-		
-		menu.add(0, 1, 0, getString(R.string.PBBMapMenu_myLocation)).setIcon(android.R.drawable.ic_menu_mylocation);
-		menu.add(0, 2, 0, getString(R.string.PBBMapMenu_changeView)).setIcon(android.R.drawable.ic_menu_mapmode);
-		menu.add(0, 3, 0, getString(R.string.PBBMapMenu_refresh)).setIcon(R.drawable.ic_menu_refresh);
-			
-		return true;
-	}
-	
-	//Menu option selection handling
-	public boolean onOptionsItemSelected(MenuItem item){
-		switch(item.getItemId()){
-			case 1:
-				GeoPoint current_point = null;
-				if(mLatitude == 0.0) {
-					Toast.makeText(FloraCacheEasyLevel.this, getString(R.string.Alert_gettingGPS), Toast.LENGTH_SHORT).show();
-				}
-				else {
-					current_point = new GeoPoint((int)(mLatitude * 1000000), (int)(mLongitude * 1000000));
-
-					mMapController = mMapView.getController();
-					mMapController.animateTo(current_point);
-				}
-				return true;
-			case 2:
-				mMapView.setSatellite(!mMapView.isSatellite());
-				return true;
-			case 3:
-				refreshList();
-				return true;
-		}
-		return false;
-	}
-
-
-}
+		<Button
+			android:layout_width="fill_parent"
+			android:layout_height="wrap_content"
+			android:text="More info from USDA"
+			android:id="@+id/more_info"
+			android:textSize="18sp"
+			android:layout_marginBottom="10dp"
+		/>
+  	  
+  	  </LinearLayout>
+  	  
+  <!-- Done : Only for local budburst -->
+  
+  
+	  <LinearLayout
+	  	android:layout_width="fill_parent"
+	  	android:layout_height="wrap_content"
+	  	android:orientation="vertical"
+	  	android:gravity="center"
+	  	android:layout_below="@+id/upper_invisible"
+	  	android:id="@+id/lower_info"
+	  >
+	    <ProgressBar
+	  		android:gravity="center"
+	  		android:layout_width="wrap_content"
+	  		android:layout_height="wrap_content"
+	  		android:id="@+id/progressbar"
+	  		android:layout_marginTop="30dp"
+	  		android:layout_marginBottom="30dp"
+	  		android:layout_marginLeft="3dp"
+	  		android:layout_marginRight="3dp"
+	  	/>
+	  	<RelativeLayout
+	  		android:layout_width="fill_parent"
+	  		android:layout_height="wrap_content"
+	  		android:layout_marginTop="10dp"
+	  		android:gravity="center"
+	  	>
+		  	<ImageView
+		  		android:gravity="center"
+		  		android:layout_width="fill_parent"
+		  		android:layout_height="wrap_content"
+		  		android:id="@+id/webimage"
+		  		android:layout_marginTop="5dp"
+		  		android:layout_marginLeft="3dp"
+		  		android:layout_marginRight="3dp"
+		  		android:scaleType="centerCrop"
+		  	/>
+	  	</RelativeLayout>
+	  	<TextView
+	  		android:layout_width="fill_parent"
+	  		android:layout_height="wrap_content"
+	  		android:id="@+id/common_name"
+	  		android:layout_gravity="center"
+	  		android:layout_marginLeft="8dp"
+	  		android:layout_marginTop="5dp"
+	  		android:textSize="18sp"
+	  		android:textStyle="bold|italic"
+	  	/>
+	  	<TextView
+	  		android:layout_width="fill_parent"
+	  		android:layout_height="wrap_content"
+	  		android:id="@+id/science_name"
+	  		android:layout_gravity="center"
+	  		android:layout_marginLeft="8dp"
+	  		android:textSize="17sp"
+	  		android:textStyle="bold|italic"
+	  	/>
+		<TextView
+	  		android:layout_width="fill_parent"
+	  		android:layout_height="wrap_content"
+	  		android:id="@+id/credit"
+	  		android:layout_gravity="center"
+	  		android:layout_marginLeft="8dp"
+	  		android:textSize="17sp"
+	  	/>
+	  	
+	  </LinearLayout>
+	</LinearLayout>
+  </ScrollView>
+  <LinearLayout
+  	android:id="@+id/lower"
+  	android:layout_below="@+id/upper"
+	android:layout_width="fill_parent"
+	android:layout_height="60dp"
+	android:layout_weight="0.1"
+	android:gravity="center"
+	android:visibility="gone"
+	android:weightSum="1"
+	android:orientation="horizontal"
+	android:background="@color/darkgray">
+	  		
+	<Button
+	  	android:layout_width="fill_parent"
+	  	android:layout_height="wrap_content"
+	  	android:layout_weight="0.5"
+	  	android:id="@+id/to_myplant"
+	  	android:textSize="13sp"
+	  	android:text="@string/Menu_addPlant"
+	  />
+	<Button
+	  	android:layout_width="fill_parent"
+	  	android:layout_height="wrap_content"
+	  	android:layout_weight="0.5"
+	  	android:id="@+id/to_shared_plant"
+	  	android:textSize="13sp"
+	  	android:text="@string/Menu_addQCPlant"
+	  />
+  </LinearLayout>
+  </LinearLayout>
+</RelativeLayout>
